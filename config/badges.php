330ab2cad7ae45e70f83114c49cafa2359b3c9f1
Damilola Adebayo <goshensoftinc@gmail.com>
2023-09-22 21:16:28 +0100

    feat: create config files to hold default achievements and badges

---
+++ config/badges.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    "badges"    =>  [
+        [
+            "value" => 0,
+            "title" => "Beginner"
+        ],
+        [
+            "value" => 4,
+            "title" => "Intermediate"
+        ],
+        [
+            "value" => 8,
+            "title" => "Advanced"
+        ],
+        [
+            "value" => 10,
+            "title" => "Master"
+        ]
+    ]
+];
